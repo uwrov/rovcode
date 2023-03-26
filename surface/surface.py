9a175f8eeaecc454ff4ffc9dbd46d84787d38b16
AlnisS <alnis.smidchens@gmail.com>
2023-03-26 15:24:38 -0700

    Add documentation to surface.py

--- surface/surface.py
+++ surface/surface.py
@@ -9,7 +9,6 @@ import websockets
 
 
 def main():
-    # os.system('clear')
     print('surface server starting')
     core.init(interface, task)  # dirty hack to import sibling modules...
     interface.init(core, task)
@@ -18,9 +17,12 @@ def main():
     asyncio.run(serve())
 
 
-# start the surface station and interface servers ------------------------------
+# start the surface station and interface servers
+# rov_server communicates with the actual ROV
+# interface_server communicates with Godot interface running on surface station
+# gather makes an infinite loop to keep communicating with ROV and interface
 async def serve():
-    rov_server = await websockets.serve(server_handler, "", 8001)
+    rov_server = await websockets.serve(rov_server_handler, "", 8001)
     interface_server = await websockets.serve(interface.server_handler, "", 8002)
     await asyncio.gather(
         rov_server.wait_closed(),
@@ -28,15 +30,16 @@ async def serve():
     )
 
 
-async def server_handler(websocket):
+# handles incoming ("consumer") and outgoing ("producer") communication with ROV
+# see: https://websockets.readthedocs.io/en/stable/howto/patterns.html
+async def rov_server_handler(websocket):
     await asyncio.gather(
-        consumer_handler(websocket),
-        producer_handler(websocket)
+        consume_incoming_data_from_rov(websocket),
+        produce_outgoing_commands_to_rov(websocket)
     )
 
 
-# incoming data handling (sensor readings etc. from ROV) -----------------------
-async def consumer_handler(websocket):
+async def consume_incoming_data_from_rov(websocket):
     async for message in websocket:
         data = json.loads(message)
         if data['type'] == 'sensor_summary':
@@ -45,16 +48,15 @@ async def consumer_handler(websocket):
             print(f'Invalid type {data.type} for message: {message}')
 
 
-# outgoing command handling (control outputs) ----------------------------------
-async def producer_handler(websocket):
+async def produce_outgoing_commands_to_rov(websocket):
     while True:
         await asyncio.gather(
-            send_control_digest(websocket),
+            update_controls_and_send_to_rov(websocket),
             asyncio.sleep(0.1)  # limit to 10 summaries per second
         )
 
 
-async def send_control_digest(websocket):
+async def update_controls_and_send_to_rov(websocket):
     await core.update_controls()
     await websocket.send(json.dumps({
         'type': 'set_pin_pwms',
@@ -62,6 +64,5 @@ async def send_control_digest(websocket):
     }))
 
 
-# start the surface station! ---------------------------------------------------
 if __name__ == "__main__":
     main()
