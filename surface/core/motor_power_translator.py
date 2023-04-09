842ab135b84c444d41b114d060d47d25ecd7296b
AlnisS <alnis.smidchens@gmail.com>
2023-04-09 15:53:50 -0700

    Generate control matrix from rov_config.py

--- surface/core/motor_power_translator.py
+++ surface/core/motor_power_translator.py
@@ -1,20 +1,27 @@
 import numpy as np
+from .rov_config import thruster_config
+from .rov_config import rov_center_of_mass
+
 # from geometry_msgs.msg import Wrench
 
 # thruster order: ['forward_left', 'forward_right', 'forward_top', 'sideways_top', 'up_left', 'up_right']
 # looks like a reasonable max on motor power is 3.87757526
 
-tweak_roll = 0.66
-transform_mat = np.array([[0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000012e+00,  0.00000000e+00,  0.00000000e+00],
-                                [-1.00000012e+00, -1.00000012e+00, -1.00000012e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00],
-                                [ 0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  1.00000000e+00,  1.00000000e+00],
-                                [-7.90000111e-02, -7.90000111e-02,  1.14700019e-01, -0.00000000e+00,  2.35999823e-02,  2.35999823e-02],
-                                [ 0.00000000e+00, -0.00000000e+00,  0.00000000e+00,  1.14700019e-01,  1.47949994e-01 * tweak_roll, -1.48149997e-01 * tweak_roll],
-                                [ 1.47950009e-01, -1.48150012e-01,  4.99950984e-05,  1.68400049e-01,  -0.00000000e+00,  0.00000000e+00]])
+control_mat = np.zeros((6, 6))
 
-transform_inv = np.linalg.inv(transform_mat)
+# first three rows of matrix are x, y, z force contributions
+for i in range(6):
+    control_mat[0:3, i] = thruster_config[i]['orientation']
+
+# last three rows of matrix are x, y, z torque vector
+for i in range(6):
+    displacement = np.subtract(thruster_config[i]['location'], rov_center_of_mass)
+    force = thruster_config[i]['orientation']
+    control_mat[3:6, i] = np.cross(displacement, force)
+
+control_inv = np.linalg.inv(control_mat)
 
 def convert_force_and_torque_to_motor_powers(vector) -> np.array:
     input_vector = np.array([vector]).T
-    motor_powers = transform_inv @ input_vector
+    motor_powers = control_inv @ input_vector
     return motor_powers
