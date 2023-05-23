573d420f2c3fdab5031da1e92c65585995ead236
uwrov-github <uwrov@uw.edu>
2023-05-23 02:09:52 +0100

    read sensor values

--- onboard/physical/physical.py
+++ onboard/physical/physical.py
@@ -2,23 +2,31 @@ import pigpio
 import logging
 import sys
 import time
+import serial
+import busio
+import board
 
-# from Adafruit_BNO055 import BNO055
+# from adafruit_bno055 import BNO055
+import adafruit_bno055
 accelerometer, gyroscope = None, None
 pi = None
-
+bno = None
 
 def init():
-    global pi
+    global pi, bno
     pi = pigpio.pi("main", 8888)
-    # bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+    # uart = busio.UART(8, 10)
+    # i2c = busio.I2C(board.SCL, board.SDA)
+    i2c = board.I2C()
+    bno = adafruit_bno055.BNO055_I2C(i2c)
+    # bno = BNO055(serial_port='/dev/serial0', rst=12)
 
 # number = GPIO number
 # value = PWM value
 def set_pin_pwm(number, value): 
     global pi
-    print("number :" +  str(number))
-    print("value: " + str(value))
+    # print("number :" +  str(number))
+    # print("value: " + str(value))
     pi.set_servo_pulsewidth(number, value)
 
 
@@ -29,8 +37,10 @@ async def flush_pin_pwms():
 
 # updates accelerometer and gyroscope values
 async def poll_sensors():
-    # # Gyroscope data (in degrees per second):
-    # gyroscope = bno.read_gyroscope()
-    # # Accelerometer data (in meters per second squared):
-    # accelerometer = bno.read_accelerometer()
-    pass
+    global accelerometer, gyroscope
+    # Gyroscope data (in degrees per second):
+    gyroscope = bno.gyro
+    # Accelerometer data (in meters per second squared):
+    accelerometer = bno.acceleration
+    print("accel: ", accelerometer)
+    print("gyro: ", gyroscope)
