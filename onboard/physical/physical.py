08a697904adb824cb93e378bf697c6bc45aa5785
uwrov-github <uwrov@uw.edu>
2023-05-27 01:27:18 +0100

    Switched to UART from I2C

--- onboard/physical/physical.py
+++ onboard/physical/physical.py
@@ -6,7 +6,7 @@ import serial
 import busio
 import board
 
-# from adafruit_bno055 import BNO055
+#from Adafruit_BNO055 import BNO055
 import adafruit_bno055
 accelerometer, gyroscope = None, None
 pi = None
@@ -15,18 +15,19 @@ bno = None
 def init():
     global pi, bno
     pi = pigpio.pi("main", 8888)
-    # uart = busio.UART(8, 10)
-    # i2c = busio.I2C(board.SCL, board.SDA)
-    i2c = board.I2C()
-    bno = adafruit_bno055.BNO055_I2C(i2c)
-    # bno = BNO055(serial_port='/dev/serial0', rst=12)
+    # uncomment if using I2C instead of UART
+    # i2c = board.I2C()
+    # bno = adafruit_bno055.BNO055_I2C(i2c)
+
+    # UART setup
+    uart = serial.Serial("/dev/serial0")
+    bno = adafruit_bno055.BNO055_UART(uart)
 
 # number = GPIO number
 # value = PWM value
 def set_pin_pwm(number, value): 
     global pi
-    # print("number :" +  str(number))
-    # print("value: " + str(value))
+    print(number, value)
     pi.set_servo_pulsewidth(number, value)
 
 
@@ -42,5 +43,6 @@ async def poll_sensors():
     gyroscope = bno.gyro
     # Accelerometer data (in meters per second squared):
     accelerometer = bno.acceleration
+    
     print("accel: ", accelerometer)
     print("gyro: ", gyroscope)
