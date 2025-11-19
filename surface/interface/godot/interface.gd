ecaa2bd6a7550eb50d3b701056e03b7f5c007ec1
UWROV Surface Station 1 <uwrov@uw.edu>
2025-11-18 20:52:53 -0500

    "added inverse motion"

--- surface/interface/godot/interface.gd
+++ surface/interface/godot/interface.gd
@@ -363,8 +363,12 @@ func _process(delta):
 		if Input.is_action_pressed("manipulator_open"):
 			bottom_manipulator_pwm += OPEN_PWM * PWM_COEFFICIENT * 1.7
 		if Input.is_action_pressed("manipulator_left"):
+			#added the 2nd motor inverse motion
+			bottom_manipulator_pwm += 100 * PWM_COEFFICIENT
 			top_manipulator_pwm -= 100 * PWM_COEFFICIENT
 		if Input.is_action_pressed("manipulator_right"):
+			#added the 2nd motor inverse motion
+			bottom_manipulator_pwm -= 100 * PWM_COEFFICIENT
 			top_manipulator_pwm += 100 * PWM_COEFFICIENT
 			
 	
