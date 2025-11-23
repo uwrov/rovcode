510015c28148f12833ee4d48d2752263b562c697
UWROV Surface Station 1 <uwrov@uw.edu>
2025-11-23 12:00:11 -0800

    "better code"

--- surface/interface/godot/interface.gd
+++ surface/interface/godot/interface.gd
@@ -364,12 +364,12 @@ func _process(delta):
 			bottom_manipulator_pwm += OPEN_PWM * PWM_COEFFICIENT * 1.7
 		if Input.is_action_pressed("manipulator_left"):
 			#added the 2nd motor inverse motion
-			bottom_manipulator_pwm += 200 * PWM_COEFFICIENT
-			top_manipulator_pwm -= 100 * PWM_COEFFICIENT
+			bottom_manipulator_pwm -= 50 * PWM_COEFFICIENT
+			top_manipulator_pwm -= 75 * PWM_COEFFICIENT
 		if Input.is_action_pressed("manipulator_right"):
 			#added the 2nd motor inverse motion
-			bottom_manipulator_pwm -= 200 * PWM_COEFFICIENT
-			top_manipulator_pwm += 100 * PWM_COEFFICIENT
+			bottom_manipulator_pwm += 50 * PWM_COEFFICIENT
+			top_manipulator_pwm += 75 * PWM_COEFFICIENT
 			
 	
 	if Input.is_action_pressed("light_on"):
