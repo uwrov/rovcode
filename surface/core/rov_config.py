8a85865b8ba698a62687db711f131035051fe22d
25Rosario <rosario.gabrielle07@gmail.com>
2025-11-23 12:15:30 -0800

    direction

--- surface/core/rov_config.py
+++ surface/core/rov_config.py
@@ -72,7 +72,7 @@ thruster_config = [
         'orientation': [0.0, -1.0, 0.0],
         'pin': 16,
         'model': 't-200',
-        'direction': -1,
+        'direction': 1,
         'handing' : -1,
         'letter': 'E',
     },
